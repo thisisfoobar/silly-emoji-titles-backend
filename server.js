2837518e4ca271779b4c9d841fb1dba87603d701
thisisfoobar <matt.mcelreath26@gmail.com>
2024-11-13 09:12:55 -0500

    Add in logic to serve the react apps index.html for unknown routes

--- server.js
+++ server.js
@@ -3,6 +3,14 @@
 const app = require('./app');
 const { PORT } = require('./config');
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Catch-all for React client-side routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 // Start the server on port 5000
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
